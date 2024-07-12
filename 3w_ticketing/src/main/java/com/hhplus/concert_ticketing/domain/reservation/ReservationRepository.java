050f691828668e2cfb9bc6cf875b74114202835a
kiya_moon <kiyamoon@daum.net>
2024-07-12 22:38:40 +0900

    feat: 예약 테이블 스케쥴러 추가

--- 3w_ticketing/src/main/java/com/hhplus/concert_ticketing/domain/reservation/ReservationRepository.java
+++ 3w_ticketing/src/main/java/com/hhplus/concert_ticketing/domain/reservation/ReservationRepository.java
@@ -3,6 +3,8 @@ package com.hhplus.concert_ticketing.domain.reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -10,4 +12,6 @@ public interface ReservationRepository extends JpaRepository<ReservationEntity, 
     ReservationEntity save(ReservationEntity reservation);
 
     Optional<ReservationEntity> findById(Long reservationId);
+
+    List<ReservationEntity> findByExpiresAtBeforeAndStatus(Timestamp now, String active);
 }
